f5c6515911815d27393d34d832e91d1c2b92b7f4
Mustiari <mustiari97@gmail.com>
2018-10-25 08:39:57 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Buton from './Button';
+
+class App extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+      Hello World
+       </Button>
+    );
+  }
+}
+
+export default App;
