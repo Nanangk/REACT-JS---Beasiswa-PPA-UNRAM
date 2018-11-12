95a66c050a951f1121cb3a5910431fee9dd01788
MegaLaely <megalaily295@gmail.com>
2018-11-12 14:22:15 +0800

    Footer

---
+++ src/tubes/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+    render() {
+        return(
+            <div >
+                <p>@copyright 2018 - Kelompok 6 PWL  </p>
+            </div>
+        )
+    }
+}
+export default Footer
