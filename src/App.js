9317c8044764aa3a9d019828013d6fd30ccfe3c1
MegaLaily <megalaily295@gmail.com>
2018-11-19 21:57:18 +0800

    KRS

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Header from './tubes/Header';
-//import Home from './tubes/Home';
+import Home from './tubes/Home';
 import Pernyataan from './tubes/Pernyataan';
 import Footer from './tubes/Footer';
+import Krs from './tubes/Krs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <Pernyataan/>
+        <Krs/>
         <Footer/>
       </div>
     );
