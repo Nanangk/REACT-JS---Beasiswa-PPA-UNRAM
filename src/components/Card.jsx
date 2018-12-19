8f7aa2cf4013001fce0573983796f7f083af7e31
Nanangk <nanangk.ti16@gmail.com>
2018-12-19 23:19:03 +0800

    perbaiki card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,54 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Gambar from "../Assets/daftar.jpg";
 
 const styles = {
   card: {
-    maxWidth: 345,
+    Width: 275,
   },
-  media: {
-    height: 140,
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
   },
 };
 
-function MediaCard(props) {
+function CardVerif(props) {
   const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+
   return (
     <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="../Assets/daftar.jpg"
-          title="Pendaftar"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Pendaftar
-          </Typography>
-          <Typography component="p">
-            Data Mahasiswa-mahasiswa yang telah melakukan pendaftaran dan akan diseleksi
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardContent >
+        <Typography variant="h5" component="h2">
+        Pendaftar Diterima
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Jumlah
+        </Typography>
+        <Typography variant="h4">
+          80
+        </Typography>
+      </CardContent>
       <CardActions>
-        
-        <Button size="small" color="primary">
-          Lihat Daftar
-        </Button>
+        <Button size="small">Lihat Daftar</Button>
       </CardActions>
     </Card>
   );
 }
 
-MediaCard.propTypes = {
+CardVerif.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(MediaCard);
+export default withStyles(styles)(CardVerif);
