ce8f67bb8900045a302409d13cd5b6e289ea77f2
Nanangk <nanangk.ti16@gmail.com>
2019-01-20 22:59:46 +0800

    penambahan fungsi "Terima " pendaftar beasiswa

--- src/components/Pendaftar.js
+++ src/components/Pendaftar.js
@@ -8,7 +8,10 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import firebase from 'firebase';
-import pdf from '../Assets/pdf.png'
+import pdf from '../Assets/pdf.png';
+import accept from '../Assets/acc.png';
+import swal from 'sweetalert';
+import cookie from 'react-cookies';
   
 export default class Pendaftar extends React.Component {
 
@@ -22,9 +25,40 @@ export default class Pendaftar extends React.Component {
            file3 : [],
            file4 : [],
            file5 : [],
+            berkas1:'',
            table : ''
         };
        }
+
+       terima(NIM,f1,f2,f3,f4,f5){
+        swal({
+            title: "Pilih OK untuk lanjut Menerima usulan KKE",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+                var todosRef = firebase.database().ref().child("penerima").child(NIM);
+                todosRef.set({
+                   nim:NIM,
+                   berkas1:f1,
+                   berkas2:f2,
+                   berkas3:f3,
+                   berkas4:f4,
+                   berkas5:f5,
+
+                });
+               
+               
+            
+                swal("Success! Pendaftar telah diterima!", {
+                    icon: "success",
+                });
+            } else {}
+          });
+       }
+
      componentDidMount(){
         const rootRef = firebase.database().ref();
         const pasienRef = rootRef.child('pendaftaran');
@@ -43,14 +77,14 @@ export default class Pendaftar extends React.Component {
          });
            const tableList = this.state.NIM.map((NIM, index) =>
              <tr>
-              
+                 
                <td>{this.state.NIM[index]}</td>
                <td><a href={this.state.file1[index]}><b><img className="pdficon" src={pdf} alt="Surat Keterangan Tidak Menerima Beasiswa" /></b></a></td>
                <td><a href={this.state.file2[index]}><b><img className="pdficon" src={pdf} alt="Surat Keterangan Aktif Kuliah" /></b></a></td>
                <td><a href={this.state.file3[index]}><b><img className="pdficon" src={pdf} alt="Slip Gaji Orang Tua" /></b></a></td>
                <td><a href={this.state.file4[index]}><b><img className="pdficon" src={pdf} alt="Kartu Keluarga" /></b></a></td>
                <td><a href={this.state.file5[index]}><b><img className="pdficon" src={pdf} alt="KRS/KHS" /></b></a></td>
-               <td></td>
+               <td>  <img onClick={ this.terima.bind(null,this.state.NIM[index],this.state.file1[index],this.state.file2[index],this.state.file3[index],this.state.file4[index],this.state.file5[index])} className="accicon" src={accept} alt="KRS/KHS" /></td>
                
              </tr>
            );
